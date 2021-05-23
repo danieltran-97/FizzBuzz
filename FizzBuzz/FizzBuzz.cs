4e7d4f2b6fa117303e99faae9343190e580ba846
Daniel Tran <59809773+danieltran-myob@users.noreply.github.com>
2021-05-24 08:51:24 +1000

    unit tests

--- FizzBuzz/FizzBuzz.cs
+++ FizzBuzz/FizzBuzz.cs
@@ -1,14 +1,28 @@
-﻿namespace FizzBuzz
+﻿using System;
+
+namespace FizzBuzz
 {
     public class FizzBuzz
     {
         public string PrintFizzBuzz(int numberInput)
         {
-            var message = "";
+            var message = String.Empty;
+            
             if (numberInput % 3 == 0)
             {
                 message = "Fizz";
             }
+
+            if (numberInput % 5 == 0)
+            {
+                message = "Buzz";
+            }
+
+            if (numberInput % 3 == 0 && numberInput % 5 == 0)
+            {
+                message = "FizzBuzz";
+            }
+            
 
             return message;
         }
--- FizzBuzzShould.cs
+++ FizzBuzzShould.cs
@@ -5,12 +5,34 @@ namespace FizzBuzz
 {
     public class FizzBuzzShould
     {
-        [Fact]
-        public void PrintFizzBuzz()
+        [Theory]
+        [InlineData(3)]
+        [InlineData(6)]
+        public void PrintFizz(int number)
         {
             var fizzBuzz = new FizzBuzz();
-            var message = fizzBuzz.PrintFizzBuzz(3);
+            var message = fizzBuzz.PrintFizzBuzz(number);
             Assert.Equal("Fizz", message);
+        }
+        
+        [Theory]
+        [InlineData(5)]
+        [InlineData(10)]
+        public void PrintBuzz(int number)
+        {
+            var fizzBuzz = new FizzBuzz();
+            var message = fizzBuzz.PrintFizzBuzz(number);
+            Assert.Equal("Buzz", message);
+        }
+        
+        [Theory]
+        [InlineData(15)]
+        [InlineData(30)]
+        public void PrintFizzBuzz(int number)
+        {
+            var fizzBuzz = new FizzBuzz();
+            var message = fizzBuzz.PrintFizzBuzz(number);
+            Assert.Equal("FizzBuzz", message);
         }
     }
 }
