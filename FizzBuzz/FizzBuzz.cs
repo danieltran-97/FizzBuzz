8e08c93aa6761c41ec704e0f6294c5b1b58b63df
Daniel Tran <59809773+danieltran-myob@users.noreply.github.com>
2021-05-25 15:05:57 +1000

    if else and for loop

--- FizzBuzz/FizzBuzz.cs
+++ FizzBuzz/FizzBuzz.cs
@@ -6,24 +6,26 @@ namespace FizzBuzz
     {
         public string PrintFizzBuzz(int numberInput)
         {
-            var message = String.Empty;
-            
-            if (numberInput % 3 == 0)
+            string message;
+
+            if (numberInput % 3 == 0 && numberInput % 5 != 0)
             {
                 message = "Fizz";
             }
-
-            if (numberInput % 5 == 0)
+            else if (numberInput % 5 == 0 && numberInput % 3 != 0)
             {
                 message = "Buzz";
             }
 
-            if (numberInput % 3 == 0 && numberInput % 5 == 0)
+            else if (numberInput % 3 == 0 && numberInput % 5 == 0)
             {
                 message = "FizzBuzz";
             }
+            else
+            {
+                message = numberInput.ToString();
+            }
             
-
             return message;
         }
     }
---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace FizzBuzz
+{
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+            var fizzBuzz = new FizzBuzz();
+            
+            for (var i = 1; i < 101; i++)
+            {
+                Console.WriteLine(fizzBuzz.PrintFizzBuzz(i));
+            }
+        }
+    }
+}
